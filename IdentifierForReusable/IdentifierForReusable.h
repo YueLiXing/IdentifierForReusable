08056c7e5645a63dde3c04963785fbdf0ea00148
yuelixing <1223343444@qq.com>
2016-12-26 16:32:32 +0800

    修改文件位置

--- IdentifierForReusable/IdentifierForReusable.h
+++ IdentifierForReusable/IdentifierForReusable.h
@@ -11,20 +11,67 @@
 #define IdentifierForReusableHeaderCode + (NSString *)identifierForReusable;\
 - (NSString *)identifierForReusable;
 
-@interface UITableViewCell (identifierForReusable)
+@interface UITableViewCell (IdentifierForReusable)
 
 IdentifierForReusableHeaderCode
 
 @end
 
-@interface UICollectionReusableView (identifierForReusable)
+@interface UICollectionReusableView (IdentifierForReusable)
 
 IdentifierForReusableHeaderCode
 
 @end
 
-@interface UITableViewHeaderFooterView (identifierForReusable)
+@interface UITableViewHeaderFooterView (IdentifierForReusable)
 
 IdentifierForReusableHeaderCode
+
+@end
+
+
+// Helpful
+
+@interface UITableView (IdentifierForReusable)
+
+/**
+ 使用类名注册 Cell
+ */
+- (void)registerCellClass:(Class)cellClass;
+/**
+ 使用类名注册 Header Footer
+ */
+- (void)registerHeaderFooterClass:(Class)aClass;
+
+- (__kindof UITableViewCell *)dequeueReusableCellWithClass:(Class)cellClass;
+
+- (__kindof UITableViewCell *)dequeueReusableCellWithClass:(Class)cellClass forIndexPath:(NSIndexPath *)indexPath;
+
+- (__kindof UITableViewHeaderFooterView *)dequeueReusableHeaderFooterViewWithClass:(Class)aClass;
+
+@end
+
+
+@interface UICollectionView (IdentifierForReusable)
+/**
+ 使用类名注册 Cell
+ */
+- (void)registerClass:(Class)cellClass;
+/**
+ 使用类名注册 Header Footer
+ */
+- (void)registerClass:(Class)aClass forSupplementaryViewOfKind:(NSString *)kind;
+
+/**
+ 获取 cell
+
+ @param cellClass cell 的 class
+ */
+- (__kindof UICollectionViewCell *)dequeueReusableCellWithReuseClass:(Class)cellClass forIndexPath:(NSIndexPath *)indexPath;
+
+/**
+ 获取 UICollectionReusableView
+ */
+- (__kindof UICollectionReusableView *)dequeueReusableSupplementaryViewOfKind:(NSString *)elementKind withReuseClass:(Class)aClass forIndexPath:(NSIndexPath *)indexPath;
 
 @end
